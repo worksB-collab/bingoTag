c95bf244e9132bb6803208bda071ec2de67c56c5
Billy Wu <billy31103@gmail.com>
2024-01-27 01:12:49 +0800

    removed comments

--- src/component/BingoGenerator.jsx
+++ src/component/BingoGenerator.jsx
@@ -14,7 +14,6 @@ const BingoGenerator = () => {
     const {width, height} = useWindowSize();
 
     useEffect(() => {
-        // Set up the interval to update the current time
         const interval = setInterval(() => {
             setCurrentTime(new Date());
         }, 1);
@@ -23,15 +22,12 @@ const BingoGenerator = () => {
         return () => clearInterval(interval);
     }, []);
 
-    // Function to format time components as two digits
     const formatTwoDigits = (number) => number.toString().padStart(2, '0');
 
-    // Function to sum the digits of time components
     const sumDigits = (time) => {
         return time.split('').reduce((sum, digit) => sum + parseInt(digit, 10), 0);
     };
 
-    // Get time components
     const year = currentTime.getFullYear().toString();
     const month = formatTwoDigits(currentTime.getMonth() + 1);
     const day = formatTwoDigits(currentTime.getDate());
@@ -39,11 +35,7 @@ const BingoGenerator = () => {
     const minutes = formatTwoDigits(currentTime.getMinutes());
     const seconds = formatTwoDigits(currentTime.getSeconds());
     const milliseconds = currentTime.getMilliseconds().toString();
-
-    // Format the full date and time
     const formattedTime = `${year}-${month}-${day} \n ${hours}:${minutes}:${seconds}.${milliseconds}`;
-
-    // Concatenate all time components and calculate the sum of digits
     const timeString = year + month + day + hours + minutes + seconds + milliseconds;
     const sumOfTimeDigits = sumDigits(timeString);
 
